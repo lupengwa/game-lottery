7a9437db28eefb3e59451fc52d7233e8676109c6
caozhang125 <steve.wang.apple@gmail.com>
2024-02-19 21:44:34 -0800

    pick a number randomly each time

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,62 @@
-import * as React from "react"
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
+  Button,
   Grid,
-  theme,
+  Text,
+  theme, Heading,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
+import {useEffect, useState} from "react";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App = () => {
+  const [nums, setNums] = useState<number[]>([]);
+  const [result, setResult] = useState('Lottery Ready');
+  const spin = () => {
+    nums.forEach(n => console.log(n));
+    let max = nums.length;
+    let min = 0;
+    let randIdx = Math.floor(Math.random()*(max-min)+ min);
+    let content =  String(nums[randIdx]);
+    let updatedNums = nums.filter(n => n !== nums[randIdx])
+    if(nums.length === 0) {
+      content = 'Lottery Ends';
+    }
+    setNums(updatedNums);
+    setResult(content);
+  }
+  const reset = () => {
+    let nums: number[] = [];
+    const end = 5;
+    for(let i = 1; i <= end; i++) {
+      nums.push(i);
+    }
+    setNums(nums);
+    setResult('Lottery Ready')
+  }
+
+  useEffect(() => {
+    reset();
+  },[]);
+
+  return (
+      <ChakraProvider theme={theme}>
+        <Box textAlign="center" fontSize="xl">
+          <Grid minH="100vh" p={3}>
+            <ColorModeSwitcher justifySelf="flex-end"/>
+            <Box>
+               <Heading as='h1' size='4xl' noOfLines={1}> {result} </Heading>
+            </Box>
+            <Box alignItems='center'>
+              <Button onClick={spin}> Spin </Button>
+              <Button onClick={reset}> Reset </Button>
+            </Box>
+          </Grid>
+        </Box>
+      </ChakraProvider>
+  );
+}
+
+
+
